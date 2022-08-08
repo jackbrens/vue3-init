78cd8582421e628a86ce8d8104340345810a998e
JackBrens <574950259@qq.com>
2022-08-08 22:06:29 +0800

    [chore] 新增postcss最小单位设置和启动时添加本地地址id

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,7 @@ module.exports = {
     // flexible配置
     'postcss-pxtorem': {
       rootValue: 192, // 设计稿宽度的1/10
+      minPixelValue: 16, //minPixelValue的选项，我设置了16，意思是所有小于16px的样式都不被转换
       propList: ['*'], // 需要做转化处理的属性，如`hight`、`width`、`margin`等，`*`表示全部
       unitPrecision: 5,
     },
--- vite.config.ts
+++ vite.config.ts
@@ -22,5 +22,6 @@ export default defineConfig({
   },
   server: {
     port: 3030,
+    host: '0.0.0.0',
   },
 })
